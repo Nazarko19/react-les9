72fa163e7377d13d5fc62b14db0b6126e58644dc
Nazarko19 <suhorukyinazar@gmail.com>
2021-08-17 15:53:06 +0300

    homework4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import Cars from "./components/cars/Cars";
-import Form from "./components/form/Form";
-import './components/cars/Cars.css'
-import './components/form/Form.css'
 
 export default function App() {
   return (
     <div>
-        <Form/>
-        <Cars/>
+
     </div>
   );
 }
--- src/components/car/Car.js
+++
@@ -1,8 +0,0 @@
-export default function Car({item}) {
-  return (
-    <div>
-       model-{item.model} price-{item.price} year-{item.year}
-
-    </div>
-  );
-}
